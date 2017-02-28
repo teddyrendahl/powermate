e8f7ba0605d00bc984048f465977828d6e40ae41
trendahl <teddy.rendahl@gmail.com>
2017-02-27 21:44:58 -0800

    Added hot/cold example

---
+++ examples/hot_cold.py
@@ -0,0 +1,64 @@
+"""
+An example use of the PowerMateBase to play a game of Hot/Cold
+
+Spin the powermate and watch the LED as it glows brighter the closer you get to
+the target value. Press and rotate the wheel to increase the speed of travel.
+Finally to exit hold and release the button for greater than two seconds
+"""
+
+import math
+import asyncio
+import argparse
+from powermate import LedEvent, PowerMateBase
+
+class HotPowerMate(PowerMateBase):
+
+    press_multipler = 2
+
+    def __init__(self, path, target=84):
+        super().__init__(path)
+
+        #Internal Warmth Parameters
+        self.value    = 0
+        self.target   = target
+
+        #Set the LED to the starting brightness
+        self._stream.send(self.warmth)
+
+
+    @asyncio.coroutine
+    def rotate(self, value, pressed=False):
+        """
+        Reimplementation of rotate method to reset the LED on every rotation.
+        If the button is pressed while rotated move twice as fast
+        """
+        if pressed:
+            value *= self.press_multiplier
+
+        self.value += value
+        return self.warmth
+
+
+    @asyncio.coroutine
+    def released(self, elapsed):
+        """
+        Reimplementation of button release to stop the demo if button is
+        pressed for more than 2 seconds
+        """
+        if elapsed > 2000:
+            return Event.stop()
+
+
+    @property
+    def warmth(self):
+        """
+        The LedEvent based on the distance from the target
+        """
+        d = math.fabs(self.value - self.target)
+        return LedEvent.percent(LedEvent.MAX_BRIGHTNESS*(1. - d/self.target))
+
+
+
+if __name__ == '__main__':
+    pm = HotPowerMate(path)
+    pm()
