aeef9bf4b1b6eeac8bb25c8d996fb469be002245
teddyrendahl <trendahl@slac.stanford.edu>
2017-09-09 00:00:22 -0700

    TST: Properly express Event in binary for testing

--- tests/test_event.py
+++ tests/test_event.py
@@ -1,30 +1,32 @@
-import pytest
 import powermate
 
+raw_evt = b'\x8f\n\x16\x00\x00\x00\x00\x00\x84\x10\x18'\
+          b'\xb6\x00\x00\x00\x00\x01\x00\x00\x01\x01\x00'\
+          b'\x00\x00'
+
 def test_stop():
+    #Generate a STOP Event
     evt = powermate.Event.stop()
     assert evt.type == powermate.event.EventType.STOP
 
 def test_raw():
+    #Convert an event back to binary
    evt = powermate.Event(1444495,3055030404,
                          powermate.event.EventType.PUSH,
                          256, 1)
-   assert evt.raw ==  '\x8f\n\x16\x00\x00\x00\x00\x00\x84\x10\x18'\
-                      '\x00\x00\x00\x00\x00\x01\x00\x00\x01\x01\x00'\
-                      '\x00\x00' 
-
+   assert evt.raw == raw_evt
+                      
 def test_from_raw():
-    evt = powermate.Event.from_raw( '\x8f\n\x16\x00\x00\x00\x00\x00'\
-                                    '\x84\x10\x18\xb6\x00\x00\x00\x00'\
-                                    '\x01\x00\x00\x01\x01\x00\x00\x00')
+    #Convert binary to an Event
+    evt = powermate.Event.from_raw(raw_evt) 
     assert evt.tv_sec  == 1444495
     assert evt.tv_usec == 3055030404
     assert evt.type    == powermate.event.EventType.PUSH
     assert evt.code    == 256
     assert evt.value   == 1
 
-
 def test_pulse():
+    #Generate a pulse Event
     evt = powermate.LedEvent.pulse()
     assert evt.speed == 255
     assert evt.asleep == 1
@@ -33,45 +35,33 @@ def test_pulse():
     assert evt.type == powermate.event.EventType.MISC
 
 def test_max():
+    #Generate an Event for the maximum brightness of the LED
     evt = powermate.LedEvent.max()
     assert evt.brightness == 255
     assert evt.type == powermate.event.EventType.MISC
 
 def test_off():
+    #Generate an Event to turn the LED off
     evt = powermate.LedEvent.off()
     assert evt.brightness == 0
     assert evt.type == powermate.event.EventType.MISC
 
 def test_percent():
+    #All the way off
     evt = powermate.LedEvent.percent(0)
     assert evt.brightness == 0
     assert evt.type == powermate.event.EventType.MISC
-
+    #Half way on
     evt = powermate.LedEvent.percent(50)
     assert evt.brightness == 128
-    
+    #Quarter of the way on
     evt = powermate.LedEvent.percent(25)
     assert evt.brightness == 64
 
 def test_socket_send(pseudo_socket):
+    #Send an event to a binary file
     evt = powermate.Event.stop()
     pseudo_socket.send(evt)
     pseudo_socket._input.seek(0)
+    #Assert we translated the whole Event
     assert pseudo_socket._input.read() == evt.raw
-
-
-def test_generator(pseudo_socket):
-    evt_1 = powermate.Event(141244, 2342340,
-                            powermate.event.EventType.PUSH,
-                            256, 1)
-    evt_2 = powermate.Event(141344, 234440,
-                            powermate.event.EventType.ROTATE,
-                            0, 2)
-    evt_3 = powermate.Event.stop()
-    for evt in (evt_1, evt_2, evt_3):
-        pseudo_socket._input.write(evt.raw)
-    assert pseudo_socket.stream.send(None) == None
-    pseudo_socket._input.seek(0)
-    assert evt_1 == pseudo_socket.stream.send(None)
-    assert evt_2 == pseudo_socket.stream.send(None)
-    assert evt_3 == pseudo_socket.stream.send(None)
