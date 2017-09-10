f1d03032df9aa5e49958ada37c0859911689a645
teddyrendahl <trendahl@slac.stanford.edu>
2017-09-09 22:36:54 -0700

    DEP: Deprecate PowermateIsRunning Exception

--- powermate/errors.py
+++ powermate/errors.py
@@ -3,9 +3,3 @@ class EventNotImplemented(NotImplementedError):
     Special exception type for non-implemented events.
     """
     pass
-
-class PowermateIsRunning(Exception):
-    """
-    Exception raised when send function called at an inappropriate time
-    """
-    pass
